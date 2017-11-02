75a41a5234716e3cf94e1ffd8d32f6577a1d1581
ursa16180 <ursa.pertot@gmail.com>
2017-11-03 00:21:28 +0100

    Counting variables

--- readDimacs.py
+++ readDimacs.py
@@ -1,8 +1,10 @@
 from boolean import *
+import operator
 
 def readDimacs(input):
     file = open(input, 'r')
     formulaAsList = []
+    dictFrequency = {}
 
     for line in file:
         first_sign = line[0]
@@ -15,12 +17,24 @@ def readDimacs(input):
             for number in listOfNumbers:
                 if number > 0:
                     listOfVariables.append(Variable(number))
+                    if number in dictFrequency:# Lahko nardiva tut samo en if, zarad lepše kode, ne nujno bolšega časa
+                        dictFrequency[number] += 1
+                    else:
+                        dictFrequency[number] = 1
                 else:
-                    listOfVariables.append(Not(Variable(abs(number))))
+                    number2 = abs(number)
+                    listOfVariables.append(Not(Variable(number2)))
+                    if number2 in dictFrequency:
+                        dictFrequency[number2] += 1
+                    else:
+                        dictFrequency[number2] = 1
             formulaAsList.append(Or(*(tuple(listOfVariables))))
     formula = And(*(tuple(formulaAsList)))
+    tupleFrequency = sorted(dictFrequency.items(), key=operator.itemgetter(1), reverse=True) #Vrne seznam tuplov(spremenljivka, število ponovitev) od najpogostejših pada
+    #print(dictFrequency) #TODO nej returna namesto printa
+    print(tupleFrequency) ##TODO return namesto printa
     return formula
 
 
 ##Test
-    # readDimacs("Examples/sudoku_easy.txt")
+#readDimacs("Examples/sudoku_easy.txt")
