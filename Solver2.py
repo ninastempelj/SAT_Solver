d0ec9534658868c6d81c7a26e2a404c83d2997cc
Nina Stempelj <nina.stempelj@gmail.com>
2017-11-05 17:07:53 +0100

    Refresh

--- Solver2.py
+++ Solver2.py
@@ -12,54 +12,53 @@ import operator
 import copy
 import time
 
-import time
-start_time = time.time()
+setValuations = set()
 
-setValuations=set()
 
 def main(vhod, izhod):
     start_time = time.time()
-    formula = readDimacs(vhod)
+    formula = read_dimacs(vhod)
     resitev = dpll(formula)
-    koncnaResitev = str()
+    koncna_resitev = str()
     for element in resitev:
-        koncnaResitev = koncnaResitev + "{} ".format(element)
-    print(koncnaResitev)
-    print ("time elapsed: {:.2f}s".format(time.time() - start_time))
-    file = open(izhod,"w")
-    file.write(koncnaResitev)##TODO formula
+        koncna_resitev += "{} ".format(element)
+    print(koncna_resitev)
+    print("time elapsed: {:.2f}s".format(time.time() - start_time))
+    file = open(izhod, "w")
+    file.write(koncna_resitev)
     file.close()
 
 
-
 def step12(formula):
-    """ Funkcija formulo poenostavi, vrne pa True in novo formulo, če je spremembna potrebna, sicer False in staro formulo"""
-
+    """ Funkcija formulo poenostavi, vrne pa True in novo formulo,
+    če je spremembna potrebna, sicer False in staro formulo"""
     changed = False
     changes = set()
-    #print(" Step12")
+    #print(formula)
     if isinstance(formula, Variable):
-        return changed, formula.simplify(), formula
+        print(str(formula) + " dodal Variable1 step12")
+        return changed, formula.simplify(), {formula}
     if len(formula.terms) == 1:
-        return changed, formula.simplify(), 0
+        return changed, formula.simplify(), set()
     for ali in formula.terms:
         tip = type(ali)
         if tip == Variable:
-            formula.simplify_by(ali)
-            changed = True
-            print(str(ali) + " dodal Variable step12")
-            changes.add(ali)
+            if ali.x not in {T,F}:
+                formula.simplify_by(ali)
+                changed = True
+                print(str(ali) + " dodal Variable2 step12")
+                changes.add(ali)
         elif tip == Not:
-            formula.simplify_by(ali)
-            changed = True
-            print(str(ali) + " dodal Not step12")
+            if not ali.x in {T, F}:
+                formula.simplify_by(ali)
+                changed = True
+                print(str(ali) + " dodal Not step12")
             changes.add(ali)
         elif len(ali.terms) == 1:
             for term in ali.terms:
                 if not term == T or term == F:
                     formula.simplify_by(term)
                     changed = True
-                    print(str(term) + " dodal term step12")
                     changes.add(term)
     return changed, formula.simplify(), changes
 
@@ -68,17 +67,12 @@ def random_literal(formula):
     if isinstance(formula, Variable) | isinstance(formula, Not):
         return formula
     for term in formula.terms:
-        #print(term)
         if isinstance(term, Variable) | isinstance(term, Not):
             return term
-       # print("ni oknc")
         return random_literal(term)
 
 
 def dpll(stara_formula, valuation=set()):
-    #print(" začetek dpll" )
-    #print(valuation)
-
     changed, formula, changes = step12(stara_formula.simplify())
     if not changed:
         valuation = valuation | changes
@@ -86,82 +80,79 @@ def dpll(stara_formula, valuation=set()):
         while changed:
             valuation = valuation | changes
             changed, formula, changes = step12(formula)
-    #print(str(formula) + " korak3 dpll")
     if formula == T:
         return valuation
     if formula == F:
         return None
-    literal = MOMS(formula)
+    literal = moms(formula)
 
     formula1 = copy.deepcopy(formula)
-    #print(str(type(literal)) + " " + str(type(formula1)))
     formula1.simplify_by(literal)
-    #print(str(formula1) + " po simplify dpll")
-    valuation1 = copy.deepcopy(valuation) # a je to ok kopirano?
+    valuation1 = copy.deepcopy(valuation)
     valuation1.add(literal)
     print(str(literal) + "dodal v dpll2")
     result1 = dpll(formula1, valuation1)
     if result1 is None:
-        #print("ugotovu da ne gre")
         formula2 = copy.deepcopy(formula)
-        formula2.simplify_by(Not(literal).flatten()) # flatten zato, da nimamo dvojne negacije
-        valuation2 = copy.deepcopy(valuation) # a je to ok kopirano
+        formula2.simplify_by(Not(literal).flatten())  # flatten zato, da nimamo dvojne negacije
+        valuation2 = copy.deepcopy(valuation)
         print(str(literal) + "dodal v dpll2")
         valuation2.add(Not(literal).flatten())
-        #valuation2[str(literal)] = False
         result2 = dpll(formula2, valuation2)
         if result2 is None:
             return None
         else:
-            return result2 #TODO nared niz
+            return result2
     else:
         return result1
 
-def readDimacs(input):
-    file = open(input, 'r')
-    formulaAsList = []
+
+def read_dimacs(vhod):
+    file = open(vhod, 'r')
+    formula_as_list = []
     for line in file:
         first_sign = line[0]
         if first_sign == "p" or first_sign == "c":
             pass
         else:
-            listOfVariables = []
-            listOfNumbers = list(map(int, line.strip().split(" ")))  ## Vrne seznam števil
-            listOfNumbers.remove(0) ##Odstrani ničlo na koncu
-            for number in listOfNumbers:
+            list_of_variables = []
+            list_of_numbers = list(map(int, line.strip().split(" ")))  # Vrne seznam števil
+            list_of_numbers.remove(0)  # Odstrani ničlo na koncu
+            for number in list_of_numbers:
                 if number > 0:
-                    listOfVariables.append(Variable(number))
+                    list_of_variables.append(Variable(number))
                 else:
                     number2 = abs(number)
-                    listOfVariables.append(Not(Variable(number2)))
-            formulaAsList.append(Or(*tuple(listOfVariables)))
-    formula = And(*tuple(formulaAsList))
+                    list_of_variables.append(Not(Variable(number2)))
+            formula_as_list.append(Or(*tuple(list_of_variables)))
+    formula = And(*tuple(formula_as_list))
     file.close()
     return formula
 
-def MOMS(formula):
-    existanceOf2 = False
+
+def moms(formula):
+    existence_of_2 = False
     if isinstance(formula, Variable) | isinstance(formula, Not):
         return formula
     elif not isinstance(formula, And):
         print(formula)
         raise NameError("Ni and v MOMSiju")
     else:
-        dictFrequency ={}
+        dict_frequency = {}
         for term in formula.terms:
-            if len(term.terms)==2:
-                existanceOf2 = True
+            if len(term.terms) == 2:
+                existence_of_2 = True
                 for termsek in term.terms:
-                    if termsek in dictFrequency:
-                        dictFrequency[termsek] += 1
+                    if termsek in dict_frequency:
+                        dict_frequency[termsek] += 1
                     else:
-                        dictFrequency[termsek] = 1
-        if existanceOf2:
-            mostCommon = tupleFrequency = sorted(dictFrequency.items(), key=operator.itemgetter(1), reverse=True)[0][0]
-            return mostCommon
+                        dict_frequency[termsek] = 1
+        if existence_of_2:
+            most_common = sorted(dict_frequency.items(), key=operator.itemgetter(1), reverse=True)[0][0]
+            return most_common
         else:
-            return(random_literal(formula))
+            return random_literal(formula)
 
 
-#main("Examples/tester.txt", "Examples/tester_r.txt")
-main("Examples/sudoku_easy.txt", "Examples/sudoku_easy_r.txt")
+# main("Examples/tester.txt", "Examples/tester_r.txt")
+main("Examples/primer3.txt", "Examples/primer3_r.txt")
